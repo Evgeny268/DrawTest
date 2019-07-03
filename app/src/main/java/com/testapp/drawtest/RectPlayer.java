e50e0c6b1010b889f0f7fd0a4cf14ac08f4cb29c
skylark71 <zher.98@mail.ru>
2019-07-03 21:05:06 +0300

    Добавил класс RectPlayer

---
+++ app/src/main/java/com/testapp/drawtest/RectPlayer.java
@@ -0,0 +1,15 @@
+package com.testapp.drawtest;
+
+import android.graphics.Canvas;
+
+public class RectPlayer implements GameObject {
+    @Override
+    public void draw(Canvas canvas) {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
