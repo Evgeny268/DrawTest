fb3656196a688f5d1efd55f363fad738d84c2b30
skylark71 <zher.98@mail.ru>
2019-07-03 20:44:49 +0300

    Изменили extend и setContent

--- app/src/main/java/com/testapp/drawtest/MainActivity.java
+++ app/src/main/java/com/testapp/drawtest/MainActivity.java
@@ -1,19 +1,22 @@
 package com.testapp.drawtest;
 
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     DrawView drawView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        drawView = findViewById(R.id.drawView);
-        setContentView(R.layout.activity_main);
-        drawView.invalidate();
+       // drawView = findViewById(R.id.drawView);
+        //setContentView(R.layout.activity_main);
+        setContentView(new DrawView(this));
+
+        //drawView.invalidate();
         //setContentView(new DrawView(this));
     }
 
