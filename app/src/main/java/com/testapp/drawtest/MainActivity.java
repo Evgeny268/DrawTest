5e43f71a94b1c54925c1976404a9cf87de8803c1
skylark71 <zher.98@mail.ru>
2019-07-03 20:52:12 +0300

    Добавил getWindow и requestWindowFeature

--- app/src/main/java/com/testapp/drawtest/MainActivity.java
+++ app/src/main/java/com/testapp/drawtest/MainActivity.java
@@ -4,6 +4,8 @@ package com.testapp.drawtest;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends Activity {
 
@@ -12,6 +14,8 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
        // drawView = findViewById(R.id.drawView);
         //setContentView(R.layout.activity_main);
         setContentView(new DrawView(this));
