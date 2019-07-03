ce06c4663f8e1fee2a8e2054be78b79bb57f6128
skylark71 <zher.98@mail.ru>
2019-07-03 19:40:47 +0300

    Изменил класс DrawView наследовал от SurfaceView

--- app/src/main/java/com/testapp/drawtest/DrawView.java
+++ app/src/main/java/com/testapp/drawtest/DrawView.java
@@ -6,85 +6,38 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
+import android.support.annotation.MainThread;
 import android.text.TextPaint;
 import android.util.AttributeSet;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 
 
-public class DrawView extends View {
+public class DrawView extends SurfaceView implements SurfaceHolder.Callback {
 
-    private TextPaint mTextPaint;
-    private float mTextWidth;
-    private float mTextHeight;
+    private MainThread thread;
 
-    public DrawView(Context context) {
+    public DrawView(Context context)
+    {
         super(context);
-        init(null,0);
-    }
 
-    @Override
-    public void invalidate() {
-        super.invalidate();
-    }
 
+    }
 
 
     @Override
-    protected void onDraw(Canvas canvas) {
-        canvas.drawColor(Color.CYAN);
-        float startX;
-        float stopX;
-        float startY;
-        float stopY;
-
-        int width = canvas.getWidth();
-        int height = canvas.getHeight();
-
-        int gridSize = 7;
-        int gridSpacing = Math.min(width, height) / gridSize;
-        int boardSize = gridSize * gridSpacing;
-
-        int xOffset = (width - boardSize)/2;
-        int yOffset = (height - boardSize)/2;
-
-        //Vertical Grid-lines
-        for (int i = 0; i < gridSize; i++) {
-
-            startX = xOffset + i*gridSpacing;
-            startY = yOffset;
-
-            stopX = startX;
-            stopY = startY + boardSize;
-
-            canvas.drawLine(startX, startY, stopX, stopY, new Paint());
-
-        }
-
-        //Horizontal Grid-lines
-        for (int i = 0; i < gridSize; i++) {
-
-            startX = xOffset;
-            startY = yOffset + i*gridSpacing;
-
-            stopX = startX + boardSize;
-            stopY = startY;
-
-            canvas.drawLine(startX, startY, stopX, stopY, new Paint());
-        }
-    }
-
-    public DrawView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        init(attrs,0);
-    }
-
-    public DrawView(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        init(attrs, defStyleAttr);
-    }
-
-    private void init(AttributeSet attrs, int defStyle){
+    public void surfaceCreated(SurfaceHolder holder) {
 
     }
 
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
 }
